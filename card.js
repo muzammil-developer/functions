ce359212afde4789471a679f3738f404bdaff971
Muzammil Hussain <itz.muzz787@gmail.com>
2026-08-28 12:40:07 +0500

    Add card.js with alert loops and image functions

---
+++ card.js
@@ -0,0 +1,31 @@
+var i = 0;
+while (i <= 3) {
+  alert(i);
+  i++;
+}
+
+
+
+
+var i = 0;
+do {
+  alert(i);
+  i++;
+} while (i <= 3);
+
+
+
+function greetUser(user) {
+    alert('Good Morning!' + user)
+}
+
+function mouseOver(image) {
+    image.src = 'card.jpg'
+}
+
+function mouseOut(image) {
+image.src = 'images.jpg'
+}
+
+
+
